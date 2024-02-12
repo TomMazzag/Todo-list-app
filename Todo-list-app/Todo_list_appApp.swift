d8504a082b9458ef80f9268ea0d4f5051dacca1b
TomMazzag <itstomazz@gmail.com>
2024-02-12 16:11:58 +0000

    Initial Commit

---
+++ Todo-list-app/Todo_list_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_list_appApp.swift
+//  Todo-list-app
+//
+//  Created by Tom Mazzag on 12/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct Todo_list_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
